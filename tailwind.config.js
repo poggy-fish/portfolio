43d6e2f3bb69afa5d7f6558db668abcb2b81469d
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-04-05 12:47:23 +0200

    changed site background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,7 +26,7 @@ module.exports = {
       colors: {
         primary: "rgb(255, 156, 68)",
         primarybg: "rgba(255, 156, 68, 0.92)",
-        black: "rgb(25,25,25)",
+        black: "rgb(3,11,22)",
         textwhite: "rgba(255,255,255,0.9)",
       },
     },
